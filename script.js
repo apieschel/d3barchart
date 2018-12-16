4e4a58d778f6cc948bb71be30acc1c7ff7a66198
Glitch (hello-webpage) <none>
2018-12-16 03:48:22 +0000

    🚵🌗 Checkpoint ./script.js:2265073/3143

--- script.js
+++ script.js
@@ -6,11 +6,55 @@ const callback = function(err, data) {
     alert('Something went wrong: ' + err);
   } else {
     dataset = data.data;
-    console.log(data.data);
+    console.log(dataset);
+    
+    const w = 1000
+    const h = 300;
+    const padding = 30;
+
+    const xScale = d3.scaleLinear()
+                      .domain([0, 200])
+                      .range([padding, w - padding]);
+    const yScale = d3.scaleLinear()
+                         .domain([0, 300])
+                         .range([h - padding, padding]);
+
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    d3.select("body")
+      .append("h1")
+      .attr("id", "title")
+      .text("GDP in the United States");
+
+    const svg = d3.select(".container")
+                  .append("svg")
+                  .attr("width", w)
+                  .attr("height", h);
+
+    svg.append("g")
+        .attr("transform", "translate(0," + (h - padding) + ")")
+        .attr("id", "x-axis")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("transform", "translate(0," + (w - padding) + ")")
+        .attr("id", "y-axis")
+        .call(yAxis);
+
+    svg.selectAll("rect")
+      .data(dataset)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", (d, i) => i + 10)
+      .attr("y", 0)
+      .attr("width", 2)
+      .attr("height", 100)
+      .attr("data-date", (d) => d[0])
+      .attr("data-gdp", (d) => d[1]);
   }
 };
-
-console.log(dataset);
 
 xhr.open('GET', 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json', true);
 xhr.responseType = 'json';
@@ -23,47 +67,3 @@ xhr.onload = function() {
   }
 };
 xhr.send();
-
-const w = 500
-const h = 300;
-const padding = 30;
-
-const xScale = d3.scaleLinear()
-                  .domain([0, 200])
-                  .range([padding, w - padding]);
-const yScale = d3.scaleLinear()
-                     .domain([0, 300])
-                     .range([h - padding, padding]);
-
-const xAxis = d3.axisBottom(xScale);
-const yAxis = d3.axisLeft(yScale);
-
-d3.select("body")
-  .append("h1")
-  .attr("id", "title")
-  .text("GDP in the United States");
-
-const svg = d3.select(".container")
-              .append("svg")
-              .attr("width", w)
-              .attr("height", h);
-
-svg.append("g")
-    .attr("transform", "translate(0," + (h - padding) + ")")
-    .attr("id", "x-axis")
-    .call(xAxis);
-
-svg.append("g")
-    .attr("transform", "translate(0," + (w - padding) + ")")
-    .attr("id", "y-axis")
-    .call(yAxis);
-
-svg.selectAll("rect")
-  .data(dataset)
-  .enter()
-  .append("rect")
-  .attr("class", "bar")
-  .attr("x", 0)
-  .attr("y", 0)
-  .attr("width", 25)
-  .attr("height", 100);
