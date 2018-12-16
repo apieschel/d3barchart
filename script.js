be38b2f493e5e54fb3f4e88f0edaba07f10aae30
Glitch (hello-webpage) <none>
2018-12-16 02:42:11 +0000

    ⏳🏔 Checkpoint ./script.js:2265073/487

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 /* globals d3 */
+const w = 500
+const h = 300;
+const padding = 30;
 
 d3.select("body")
   .append("h1")
   .attr("id", "title")
   .text("GDP in the United States");
+
+const svg = d3.select("body")
+              .append("svg")
+              .attr("width", w)
+              .attr("height", h);
+
+svg.append("g")
+    .attr("transform", "translate(0," + (h - padding) + ")")
+    .call(xAxis);;
