7a3ace865cbe4fb62acd6622bd7e316e23e90196
Glitch (hello-webpage) <none>
2018-12-16 22:10:23 +0000

    🚪🎅 Checkpoint ./script.js:2265073/5471 ./style.css:2265073/28

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 /* globals d3 */
-
 let dataset = [];
 const xhr = new XMLHttpRequest();
 const callback = function(err, data) {
@@ -22,9 +21,8 @@ const callback = function(err, data) {
     console.log(dates);
     console.log(dates2);
     
-    const w = 1200
-    const barWidth = w/275;
-    const h = 750;
+    const w = 800
+    const h = 400;
     const padding = 40;
     const minX = d3.min(dates2, (d) => d);
     const maxX = d3.max(dates2, (d) => d);
@@ -45,19 +43,10 @@ const callback = function(err, data) {
     
     const gdpScale = d3.scaleLinear()
                          .domain([0, maxY])
-                         .range([0, h - padding]);
+                         .range([0, h]);
 
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
-    
-    /* Help from @bloo0178 in this forum post for passing the tooltip tests: 
-    https://www.freecodecamp.org/forum/t/issues-with-data-visualization-project-1-bar-chart-tests/222959/2 */
-    let tooltip = d3
-      .select("body")
-      .append("div")
-      .attr("class", "tooltip")
-      .attr("id", "tooltip")
-      .style("opacity", 0);
 
     d3.select("body")
       .append("h1")
@@ -84,49 +73,13 @@ const callback = function(err, data) {
       .enter()
       .append("rect")
       .attr("class", "bar")
-      .attr("x", (d) => xScale(d[2]))
+      .attr("x", (d, i) => xScale(d[2]))
       .attr("y", (d) => h - gdpScale(d[1]) - padding)
-      .attr("width", barWidth)
+      .attr("width", 2)
       .attr("height", (d) => gdpScale(d[1]))
       .attr("data-date", (d) => d[0])
-      .attr("data-gdp", (d) => d[1])
-      .attr("fill", "purple")
-      .on("mouseover", function(d) {
-        tooltip
-          .transition()
-          .duration(200)
-          .style("opacity", 0.9);
-        tooltip
-          .html("<p>Date: " + d[0] + "</p><p>GDP: " + d[1] + "</p>")
-          .style("left", d3.event.pageX + 20 + "px")
-          .style("top", d3.event.pageY + 20 + "px");
-        tooltip.attr("data-date", d[0]);
-      })
-      .on("mouseout", function(d) {
-        tooltip
-          .transition()
-          .duration(400)
-          .style("opacity", 0);
-      });
-    /*
-    svg.selectAll("div")
-       .data(dataset)
-       .enter()
-       .append("div")
-       .attr("class", "tooltip")
-       .attr("data-date", (d) => d[0])
-       .text((d) => d[0] + " " + d[1]);
-    */
-    
-     svg.selectAll("text")
-       .data(dataset)
-       .enter()
-       .append("text")
-       .attr("x", (d) => xScale(d[2]))
-       .attr("y", (d) => h - gdpScale(d[1]) - padding)
-       .text((d) => d[0])
+      .attr("data-gdp", (d) => d[1]);
   }
-  
 };
 
 xhr.open('GET', 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json', true);
