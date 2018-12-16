3589332648381c1f98ba9f9c68605cc6be1de421
Glitch (hello-webpage) <none>
2018-12-16 02:53:13 +0000

    ⛺️🍎 Checkpoint ./script.js:2265073/532 ./style.css:2265073/112 ./index.html:2265073/58

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ const w = 500
 const h = 300;
 const padding = 30;
 
+const xScale = d3.scaleLinear()
+                  .domain([0, 200])
+                  .range([padding, w - padding]);
+const xAxis = d3.axisBottom(xScale);
+
 d3.select("body")
   .append("h1")
   .attr("id", "title")
   .text("GDP in the United States");
 
-const svg = d3.select("body")
+const svg = d3.select(".container")
               .append("svg")
               .attr("width", w)
               .attr("height", h);
 
 svg.append("g")
     .attr("transform", "translate(0," + (h - padding) + ")")
+    .attr("id", "x-axis")
     .call(xAxis);;
